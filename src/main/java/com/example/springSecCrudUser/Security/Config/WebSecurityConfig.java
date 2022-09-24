28b292c51101f6e83aceaa74761dcc0bba1e2f5d
Kevin De jesus <sebadjkevin@gmail.com>
2022-09-23 23:51:52 -0300

    .

--- src/main/java/com/example/springSecCrudUser/Security/Config/WebSecurityConfig.java
+++ src/main/java/com/example/springSecCrudUser/Security/Config/WebSecurityConfig.java
@@ -1,14 +1,18 @@
 package com.example.springSecCrudUser.Security.Config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.example.springSecCrudUser.Security.SecService.impl.UserDetailsServiceImpl;
 
@@ -23,11 +27,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsconfig = new CorsConfiguration();
+        /*por el momento no hay restricciones de origenes*/
+        corsconfig.setAllowedOriginPatterns(List.of("*"));
+        corsconfig.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+        corsconfig.setAllowedHeaders(List.of("Access-Control-Allow-Origin", "X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+        corsconfig.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsconfig);
+        return source;
+    }
    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         
         http
+        .cors().and().csrf().disable()
         .authorizeRequests()
         .antMatchers("/auth/register**","/auth/login**").permitAll()
 	.antMatchers("/metadata","/favicon.ico","/*.css","/*.js").permitAll()
